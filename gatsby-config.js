f50fa0f1db65f2fc1c7ba70574403634f86b7349
Santiago Quinteros <contacto.quinteros@gmail.com>
2019-03-18 12:40:29 -0500

    Add noopener noreferrer

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
-              rel: "nofollow"
             }
           }
         ],
