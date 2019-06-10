85836c833d6c57b791be1f7573e37852046516ac
Santiago Quinteros <contacto.quinteros@gmail.com>
2019-06-10 12:30:02 +0300

    Add hotjar to /about to generate heatmaps

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -285,7 +285,21 @@ const WatchMeCodeVideo = styled.iframe`
 
 const About: React.FunctionComponent = () => (
   <IndexLayout>
-    <Helmet>22
+    <Helmet
+      script={[{
+        type: 'text/javascript',
+        innerHTML: `
+        (function(h,o,t,j,a,r){
+          h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+          h._hjSettings={hjid: 1356406,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `
+      }]} 
+    >
       <title>How to scale your node.js application to 100k users and beyond</title>
     </Helmet>
       <header css={[outer, SiteHeader]}>
